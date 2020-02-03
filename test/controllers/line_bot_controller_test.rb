8cffd558dd1c3a366b9e4fa9db16b509c4ebca08
Robert Guiscard <robert.guiscard@outlook.com>
2020-02-03 03:20:07 -0500

    remove used test

--- test/controllers/line_bot_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LineBotControllerTest < ActionDispatch::IntegrationTest
-  test "should get callback" do
-    post line_bot_callback_url
-    assert_response :success
-  end
-
-end
