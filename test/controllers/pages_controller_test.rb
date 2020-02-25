3560907b62e1375c77a14d6f352dd6df99886bef
Robert Guiscard <robert.guiscard@outlook.com>
2020-02-24 22:43:16 -0500

    update Gemfile

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,6 +1,33 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    @bit = bits(:one)
+    sign_in @user
+  end
+
+  test "should not get workspace without sign in" do
+    sign_out @user
+
+    get workspace_url
+    assert_response :redirect
+  end
+
+  test "should get workspce" do
+    get workspace_url
+    assert_response :success
+  end
+
+  test "should get home without sign in" do
+    sign_out @user
+
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
