2bf79a2d3abec45762333528570976946a26b01f
Robert Guiscard <robert.guiscard@outlook.com>
2020-02-25 22:16:23 -0500

    draw line to indicate predecessor

--- app/javascript/controllers/gantt_controller.js
+++ app/javascript/controllers/gantt_controller.js
@@ -92,7 +92,6 @@ export default class extends Controller {
           x2 += (right.offsetWidth / 2);
           y2 += right.offsetHeight+8;
           this.drawCircle(x1, y1, 3, color);
-          //this.drawCircle(x2, y2, 3, color);
           this.drawCurvedLine(x1, y1, x2, y2, color, -1*tension);
         } else {
           x1 += (left.offsetWidth / 2);
@@ -100,7 +99,6 @@ export default class extends Controller {
           x2 -= 8 // minus triangle size
           y2 += (right.offsetHeight / 2);
           this.drawCircle(x1, y1, 3, color);
-          //this.drawCircle(x2, y2, 3, color);
           this.drawLine(x1, y1, x2, y2, color, tension);
         }
       } else {
@@ -110,7 +108,6 @@ export default class extends Controller {
           x2 += (right.offsetWidth / 2);
           y2 += right.offsetHeight+8;
           this.drawCircle(x1, y1, 3, color);
-          //this.drawCircle(x2, y2, 3, color);
           this.drawCurvedLine(x1, y1, x2, y2, color, -1*tension);
         } else {
           x1 += (left.offsetWidth / 2);
@@ -118,10 +115,20 @@ export default class extends Controller {
           x2 += (right.offsetWidth / 2);
           y2 -= 8;
           this.drawCircle(x1, y1, 3, color);
-          //this.drawCircle(x2, y2, 3, color);
           this.drawCurvedLine(x1, y1, x2, y2, color, -1*tension);
         }
       }
+    } else if (!left && right) {
+      console.log('unseen predecessor')
+      // draw arrow to indicate unseen predecessor
+      var rightPos = this.findAbsolutePosition(right);
+      var x2 = rightPos.x;
+      var y2 = rightPos.y;
+      x1 = 0;
+      x2 -= 8 // minus triangle size
+      y2 += (right.offsetHeight / 2);
+      y1 = y2;
+      this.drawCurvedLine(x1, y1, x2, y2, color, tension);
     }
   }
 
